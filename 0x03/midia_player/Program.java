7cf62fa039b0bd3772d7fb4fb83fda929377a3e6
Raquel Teixeira <raquelat@ciandt.com>
2022-07-21 18:45:47 -0300

    media

--- 0x03/midia_player/Program.java
+++ 0x03/midia_player/Program.java
@@ -8,7 +8,6 @@ public class Program {
         VideoMediaPlayer videoMediaPlayer = new VideoMediaPlayer();
         videoMediaPlayer.reproduzirMP4("Uncharted.mp4");
 
-        System.out.println();
 
         audioPlayer.reproduzir(TipoMedia.VLC, "Moon Knight S01E01.vlc");
     }
